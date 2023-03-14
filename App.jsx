8e01f0eac1f47a7ad370b1f7d02a9994c888cae3
Febri Milansyah <febrimilan00@gmail.com>
2023-03-14 11:32:44 +0700

    My Cart & Product Info

--- App.jsx
+++ App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import {View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from './components/screens/Home';
+import MyCart from './components/screens/MyCart';
+import ProductInfo from './components/screens/ProductInfo';
 
 const App = () => {
   const Stack = createNativeStackNavigator();
@@ -14,6 +15,8 @@ const App = () => {
           headerShown: false,
         }}>
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="MyCart" component={MyCart} />
+        <Stack.Screen name="ProductInfo" component={ProductInfo} />
       </Stack.Navigator>
     </NavigationContainer>
   );
