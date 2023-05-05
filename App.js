47d521668378e9e709ca317d1bab43401f32aa6e
Febri Milansyah <febrimilan00@gmail.com>
2023-05-05 15:12:12 +0700

    Update Routing

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './src/screens/Home';
@@ -10,9 +10,27 @@ import OrderList from './src/screens/OrderList';
 import ProductByCategory from './src/screens/ProductByCategory';
 import AccountDetails from './src/screens/AccountDetails';
 import PaymentView from './src/screens/PaymentView';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { USER_LEVEL } from './src/database/AppData';
 
 const App = () => {
   const Stack = createNativeStackNavigator();
+  const [userData, setUserData] = useState('waiting');
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+  const getUserData = async () => {
+    try {
+      const newUserData = await AsyncStorage.getItem('user_data');
+      if (newUserData !== null) {
+        setUserData(JSON.parse(newUserData));
+      }
+    } catch (e) {
+      setUserData(null);
+    }
+  };
 
   return (
     <NavigationContainer>
@@ -20,15 +38,28 @@ const App = () => {
         screenOptions={{
           headerShown: false,
         }}>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="MyCart" component={MyCart} />
-        <Stack.Screen name="ProductInfo" component={ProductInfo} />
-        <Stack.Screen name="SignIn" component={SignIn} />
-        <Stack.Screen name="SignUp" component={SignUp} />
-        <Stack.Screen name="OrderList" component={OrderList} />
-        <Stack.Screen name="ProductByCategory" component={ProductByCategory} />
-        <Stack.Screen name="AccountDetails" component={AccountDetails} />
-        <Stack.Screen name="PaymentView" component={PaymentView} />
+        {userData?.level === 'waiting' ? (
+          <></>
+        ) : userData?.level === USER_LEVEL.OWNER ? (
+          <></>
+        ) : userData?.level === USER_LEVEL.CASHIER ? (
+          <></>
+        ) : (
+          <>
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="MyCart" component={MyCart} />
+            <Stack.Screen name="ProductInfo" component={ProductInfo} />
+            <Stack.Screen name="SignIn" component={SignIn} />
+            <Stack.Screen name="SignUp" component={SignUp} />
+            <Stack.Screen name="OrderList" component={OrderList} />
+            <Stack.Screen
+              name="ProductByCategory"
+              component={ProductByCategory}
+            />
+            <Stack.Screen name="AccountDetails" component={AccountDetails} />
+            <Stack.Screen name="PaymentView" component={PaymentView} />
+          </>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/database/AppData.js
+++ src/database/AppData.js
@@ -16,3 +16,8 @@ export const COLOR_SETTINGS = {
 };
 export const USER_PICTURE_DEFAULT =
   'https://img.freepik.com/free-photo/cute-woman-hold-hands-gesture-empty-spec-business-woman-concept-pink-background-3d-rendering_56104-1468.jpg?w=740&t=st=1685270243~exp=1685270843~hmac=4ec37e0e8f5ad0abcda82edbca3aab945c11b4a467321e2c0b90bf58b3852617';
+export const USER_LEVEL = {
+  BUYER: 'pembeli',
+  CASHIER: 'kasir',
+  OWNER: 'pemilik toko',
+};
